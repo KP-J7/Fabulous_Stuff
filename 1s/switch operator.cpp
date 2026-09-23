bb0076e2038063c172be5bec09d6c74960d2e753
KP-J7 <pospelovalara78@gmail.com>
2026-09-23 15:24:15 +0300

    Update switch operator.cpp

--- 1s/switch operator.cpp
+++ 1s/switch operator.cpp
@@ -5,12 +5,12 @@ int main() {
     int n;
     switch (n){
         case 1:
-            /**/;
+            /*код_1*/;
             break;
         case 2:
-            /**/;
+            /*код_2*/;
             break;
         default:
-           /**/;
+           /*код_default*/;
     }
 }
