4ddb45e3ce89ac3a0f6d4fed730950d79df513a8
KP-J7 <pospelovalara78@gmail.com>
2026-09-24 11:16:44 +0300

    Update input_output.cpp

--- 1s/input_output.cpp
+++ 1s/input_output.cpp
@@ -3,4 +3,6 @@ using namespace std;
 int main(){
   cout<<"1"; /*outputs 1 to the console, "cout" is  the streaming output function in C++ */
   cin>>1;/*inputs 1 into the console,"cin" is the streaming input function in C++ */
+  scanf();/* it is the input function in C*/
+  printf();/* it is the output function in C*/
 }
