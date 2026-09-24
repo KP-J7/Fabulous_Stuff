4709b07e87f4789a4a631d77557871f7e1b96c93
KP-J7 <pospelovalara78@gmail.com>
2026-09-24 11:05:22 +0300

    Create input_output.cpp

---
+++ 1s/input_output.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"1"; /*outputs 1 to the console, "cout" is  the streaming output function in C++ */
+  cin>>1;/*inputs 1 into the console,"cin" is the streaming input function in C++ */
+}
